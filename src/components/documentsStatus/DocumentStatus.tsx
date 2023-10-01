f781cd5fde1a86f07a7eba2ebaf5624e52b77be8
Martin <david.martin6@ascension.org>
2023-10-01 14:33:05 -0500

    first commit

---
+++ src/components/documentsStatus/DocumentStatus.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./DocumentStatus.css"
+
+const DocumentStatus = () => {
+    return (
+        <div className='doc-summary'>
+            <div className='doc-status'>Isolation:</div>
+            <div className='doc-status'>Complete</div>
+        </div>
+    )
+}
+export default DocumentStatus
---
+++ src/components/patientData/PatientData.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './PatientData.css'
+
+const PatientData = () => {
+    return (
+        <div>
+            <div >
+                <div>Patient Data</div>
+            </div>
+            <div>
+                <div>Name - Seamless Apple</div>
+                <div>FIN - 1111111</div>
+                <div>MRN - 1111111</div>
+            </div>
+        </div>
+    )
+}
+export default PatientData
